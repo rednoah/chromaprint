d0a6ca6fecbaf4b2a55065176f73d064a9cf0bb5
Reinhard Pointner <reinhard.pointner@gmail.com>
2020-03-02 20:34:57 +0700

    Try to create custom build

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+# reset
+git reset --hard
+git pull
+git --no-pager log --pretty=oneline -1
+
+# build
+mkdir "$PWD/dist"
+cmake -DCMAKE_INSTALL_PREFIX="$PWD/dist" -DCMAKE_BUILD_TYPE=Release -DBUILD_TOOLS=ON -DBUILD_SHARED_LIBS=OFF -DFFT_LIB=vdsp .
+make
+make install
+
+# test
+"$PWD/dist/bin/fpcalc" -version
