21fedf9b9d9807c4a49a4b603fab6e5fcf3b7a96
Reinhard Pointner <reinhard.pointner@gmail.com>
2020-03-02 22:01:48 +0700

    Try to create custom build

--- build.sh
+++ build.sh
@@ -2,18 +2,17 @@
 
 
 # reset
-git reset --hard
-git pull
-git --no-pager log --pretty=oneline -1
+git reset --hard && git pull && git --no-pager log --pretty=oneline -1
 
 # build
-export export MACOSX_DEPLOYMENT_TARGET="10.10"
+rm -rv "$PWD/dist" "CMakeCache.txt"
 mkdir "$PWD/dist"
-cmake -DCMAKE_INSTALL_PREFIX="$PWD/dist" -DCMAKE_BUILD_TYPE=Release -DBUILD_TOOLS=ON -DBUILD_SHARED_LIBS=OFF -DFFT_LIB=vdsp .
+cmake -DCMAKE_INSTALL_PREFIX="$PWD/dist" -DCMAKE_BUILD_TYPE=Release -DBUILD_TOOLS=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 -DCMAKE_OSX_ARCHITECTURES=x86_64 -DBUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS=-stdlib=libc++ -DFFT_LIB=vdsp -DFFMPEG_ROOT="$PWD/package/ffmpeg-4.2.2-audio-macos-x86_64" .
 make clean
 make
 make install
 
 # test
 "$PWD/dist/bin/fpcalc" -version
+otool -l "$PWD/dist/bin/fpcalc"
 otool -L "$PWD/dist/bin/fpcalc"
