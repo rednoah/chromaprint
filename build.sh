2f2df361c5e257decf9a5681afd3ae8f25865c6e
Reinhard Pointner <reinhard.pointner@gmail.com>
2020-03-02 20:43:09 +0700

    Try to create custom build

--- build.sh
+++ build.sh
@@ -7,8 +7,10 @@ git pull
 git --no-pager log --pretty=oneline -1
 
 # build
+export export MACOSX_DEPLOYMENT_TARGET="10.10"
 mkdir "$PWD/dist"
 cmake -DCMAKE_INSTALL_PREFIX="$PWD/dist" -DCMAKE_BUILD_TYPE=Release -DBUILD_TOOLS=ON -DBUILD_SHARED_LIBS=OFF -DFFT_LIB=vdsp .
+make clean
 make
 make install
 
